dc452f91785aa2cbdda0b5ac6945d95a779dab50
Wilhem3959 <36482878+Wilhem3959@users.noreply.github.com>
2022-10-24 12:47:02 -0400

    first commit

---
+++ NavDrone.Core/Contracts/Services/ISampleDataService.cs
@@ -0,0 +1,9 @@
+﻿using NavDrone.Core.Models;
+
+namespace NavDrone.Core.Contracts.Services;
+
+// Remove this class once your pages/features are using your data.
+public interface ISampleDataService
+{
+    Task<IEnumerable<SampleOrder>> GetListDetailsDataAsync();
+}
---
+++ NavDrone/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,8 @@
+﻿namespace NavDrone.Behaviors;
+
+public enum NavigationViewHeaderMode
+{
+    Always,
+    Never,
+    Minimal
+}
